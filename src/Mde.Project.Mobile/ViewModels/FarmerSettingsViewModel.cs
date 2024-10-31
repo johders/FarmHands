f37ca407045d1810c71f5a239cfcf625677e7b07
jd-HW <johannes.dereuddre@student.howest.be>
2024-10-31 11:01:15 +0100

    Add sign out buttons to settings pages

--- src/Mde.Project.Mobile/ViewModels/FarmerSettingsViewModel.cs
+++ src/Mde.Project.Mobile/ViewModels/FarmerSettingsViewModel.cs
@@ -105,5 +105,20 @@ namespace Mde.Project.Mobile.ViewModels
 		{
 			App.Current.MainPage = new AppShellUser();
 		});
+
+		public ICommand LogOutCommand => new Command(async () =>
+		{
+			bool isConfirmed = await ShowLogoutConfirmationAsync();
+
+			if (isConfirmed)
+			{
+				Application.Current.MainPage = new AppShellStartup();
+			}
+		});
+
+		public async Task<bool> ShowLogoutConfirmationAsync()
+		{
+			return await Application.Current.MainPage.DisplayAlert("Confirm signout", "Are you sure you want to sign out?", "Yes", "No");
+		}
 	}
 }
